c1c3d44ec38fab38c553a7d585146484068c7abc
Kaisa Koski <kaisa.m.koski@gmail.com>
2022-01-18 16:16:04 +0200

    Osa1 b-kohdan tehtävät 1.3-1.4

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,43 +1,65 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application developoment'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+]
+
   
-  return (
-    <>
-      <Header course = {course} />
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
-      <Total total={part1+part2+part3} />
-    </>
-  )
+return (
+  <div>
+    <Header course={course} />
+    <Content parts={parts} />
+    <Total parts={parts} />
+  </div>
+)
 }
 
 const Header = (props) => {
-  return (
-    <div>
+return (
+  <div>
     <h1>{props.course}</h1>
     </div>
   )
 }
 
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
+    </div>
+  )
+}
+
 const Part = (props) => {
+  return(
   <div>
-    <p>{props.part} {props.exercises} </p>
+    <p>{props.part.name} {props.part.exercises}</p>
   </div>
+  )
 }
 
 const Total = (props) => {
-  <div>
-    <p>Number of exercises {props.total}</p>
-  </div>
+  const summa = sum(props.parts[0].exercises, props.parts[1].exercises,props.parts[2].exercises )
+  return <p>Number of exercises {summa}</p>
+}
+
+function sum(p1, p2, p3) {
+  return p1 + p2 + p3
 }
 
 export default App 
