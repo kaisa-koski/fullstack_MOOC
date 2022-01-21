f50a40f425973de5f88c653f69322c7465a85068
Kaisa Koski <kaisa.m.koski@gmail.com>
2022-01-21 12:24:42 +0200

    Tehtävä 1.7 unicafe step2

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -12,6 +12,24 @@ const [good, setGood] = useState(0)
 const [neutral, setNeutral] = useState(0)
 const [bad, setBad] = useState(0)
 
+const total = (p1, p2, p3) => (p1 + p2 + p3)
+
+const average = (p1, p2, p3) => {
+  let good = p1 * 1
+  let neutral = p2 * 0
+  let bad = p3 * (-1)
+  let sum = total(p1, p2, p3)
+  if (sum === 0) return 0
+  return (good + neutral + bad) / sum
+}
+
+const positive = (p1, p2, p3) => {
+  let sum = total(p1, p2, p3)
+  if (sum === 0) return 0
+  return (good / sum) * 100
+}
+
+
 
   return (
     <div>
@@ -23,6 +41,8 @@ const [bad, setBad] = useState(0)
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>average {average(good, neutral, bad)}</p>
+      <p>positive {positive(good, neutral, bad)} %</p>
     </div>
   )
 }
