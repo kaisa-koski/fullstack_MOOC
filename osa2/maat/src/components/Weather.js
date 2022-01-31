d6e0a0f720757222803f21bc92d181315bcfdf56
Kaisa Koski <kaisa.m.koski@gmail.com>
2022-01-31 16:20:24 +0200

    2.14 viimeistelyä

--- osa2/maat/src/components/Weather.js
+++ osa2/maat/src/components/Weather.js
@@ -6,8 +6,7 @@ const Weather = ({ city }) => {
 
     const api_key = process.env.REACT_APP_API_KEY
 
-    const [weather, setWeather] = useState()
-    console.log(weather);
+    const [weather, setWeather] = useState('')
 
     const params = {
         access_key: api_key,
@@ -16,23 +15,28 @@ const Weather = ({ city }) => {
     useEffect(() => {
         axios.get('http://api.weatherstack.com/current', { params })
             .then(response => {
-                setWeather(response.data)
-                console.log(response.data) 
+                 setWeather(response.data)
             })
     }, [])
 
 
-  
-    if (weather.success === false) return ( //TODO: Ei toimi jos api_key ei toimi
-        <div>Unable to oad weather</div>
-    )
-      
-    else return (
+if (weather.hasOwnProperty('current')) return ( // TODO: Error käsittelyn voisi tehdä myöhemmin tehokkaammin/tyylikkäämmin
+    <div>
+        <h2>Weather in {city}</h2>
+        <p><strong>temperature:</strong> {weather.current.temperature} Celcius</p>
+        <p> <img src={weather.current.weather_icons[0]} alt='weather icon'/></p>
+        <p><strong>wind:</strong> {weather.current.wind_speed} mph direction {weather.current.wind_dir}</p>
+    </div>
+)
+if (weather.hasOwnProperty('error')) return (
+    <div>
+            Error when loading weather: {weather.error.info}
+        </div>
+)
+else 
+ return (
         <div>
-            <h2>Weather in {city}</h2>
-            <p><strong>temperature:</strong> {weather.current.temperature} Celcius</p>
-            <p> <img src={weather.current.weather_icons[0]} /></p>
-            <p><strong>wind:</strong> {weather.current.wind_speed} mph direction {weather.current.wind_dir}</p>
+            Loading weather...
         </div>
     )
 }
